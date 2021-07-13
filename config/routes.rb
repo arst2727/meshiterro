922ff29a3dd0c7f396c5f6bd41bf7633741a9683
arst2727 <yanagit4729@gmail.com>
2021-07-13 06:12:08 +0000

    [Finish]4-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  
+  # 不要なルーティングがされないようにonly
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
 end
