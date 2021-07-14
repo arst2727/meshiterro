fcb3e0d35fdb14946085b72cc8f03633b7eca621
arst2727 <yanagit4729@gmail.com>
2021-07-14 06:15:41 +0000

    [Finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   
@@ -7,6 +8,7 @@ Rails.application.routes.draw do
     # 単数(resource)にすると、そのコントローラのidがリクエストに含まれなくなる
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
-    
   end
+  
+  resources :users, only: [:show, :edit, :update]
 end
