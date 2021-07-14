acee05bf8a03b00b00ba7827db6a43e72cec62fb
arst2727 <yanagit4729@gmail.com>
2021-07-14 06:46:42 +0000

    [Finish]4-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 class ApplicationController < ActionController::Base
+  # ログイン認証が済んでいない状態でトップページ以外の画面にアクセスしても、ログイン画面へリダイレクト
+  # authenticate_userメソッドはdeviseのメソッド
+  before_action :authenticate_user!,except: [:top]
+  
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def after_sign_in_path_for(resource)
